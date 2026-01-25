2239c777be211703753a6cf4eea5b4b83f8b57ed
Nikolai Pedersen Olsen <Nikolaipo@uia.no>
2026-01-25 22:53:43 +0100

    started on homescreen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="newNote"
         options={{
-          title: 'Explore',
+          title: 'New note',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
